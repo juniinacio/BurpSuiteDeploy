09f62738aa52822ae4054e0e23829e641de792c6
Juni Inacio <24542013+juniinacio@users.noreply.github.com>
2020-07-08 13:00:21 +0200

    Correct assertion

--- unit/tests/public/Invoke-BurpSuiteDeployment.Tests.ps1
+++ unit/tests/public/Invoke-BurpSuiteDeployment.Tests.ps1
@@ -1211,7 +1211,7 @@ InModuleScope $env:BHProjectName {
                 Should -Invoke -CommandName New-BurpSuiteScheduleItem -ParameterFilter {
                     $SiteId -eq $testScheduleDeployment.Properties.siteId `
                         -and $ScanConfigurationIds[0] -eq $testScheduleDeployment.Properties.scanConfigurationIds[0] `
-                        -and $Schedule.InitialRunTime -gt (Get-Date -Date $testScheduleDeployment.Properties.schedule.initialRunTime) ` # TODO: Update assertion
+                        -and $Schedule.InitialRunTime -gt (Get-Date -Date $testScheduleDeployment.Properties.schedule.initialRunTime) `
                         -and $Schedule.RRule -eq $testScheduleDeployment.Properties.schedule.rRule
                 }
 
